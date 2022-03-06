ca4b06eef3cffe697de967f894e957da4f75bf1d
ohjinseo <ojs835@naver.com>
2022-03-07 01:08:39 +0900

    jwt 토큰 발급 및 유효성 검증 단위 테스트 작성

--- src/main/java/com/example/springbootlab/handler/JwtHandler.java
+++ src/main/java/com/example/springbootlab/handler/JwtHandler.java
@@ -1,9 +1,11 @@
 package com.example.springbootlab.handler;
 
 import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+@Component
 public class JwtHandler {
     private String type = "Bearer";
 
@@ -13,7 +15,7 @@ public class JwtHandler {
         return type + Jwts.builder() // jwt 빌드 시작
                 .setSubject(subject)
                 .setIssuedAt(now)   // 토큰 발급일을 지정
-                .setExpiration(new Date(now.getTime() + maxAgeSeconds * 1000L)) // 만료 일자 지정 (ms 단위로)
+                .setExpiration(new Date(now.getTime() + maxAgeSeconds * 1000L)) // 만료 일자 지정 (ms 단위이므로 1000을 곱함)
                 .signWith(SignatureAlgorithm.HS256, encodedKey) // 파라미터로 받은 key로 SHA-256알고리즘을 사용하여 서명
                 .compact(); // 토큰 생성
     }
@@ -22,6 +24,7 @@ public class JwtHandler {
         return parse(encodedKey, token).getBody().getSubject();
     }
 
+    // 토큰 생성에 사용했던 key가 맞는지 확인
     public boolean validate(String encodedKey, String token) {
         try {
             parse(encodedKey, token);
@@ -32,9 +35,9 @@ public class JwtHandler {
     }
 
     // 정보의 한 덩어리를 클레임(Claim)이라고 부름
-    private Jws<Claims> parse(String key, String token) {
+    private Jws<Claims> parse(String encodedKey, String token) {
         return Jwts.parser()
-                .setSigningKey(key) // encodedKey
+                .setSigningKey(encodedKey)
                 .parseClaimsJws(untype(token)); // Bearer를 제외한 토큰 삽입
     }
 
--- src/test/java/com/example/springbootlab/handler/JwtHandlerTest.java
+++ src/test/java/com/example/springbootlab/handler/JwtHandlerTest.java
@@ -2,11 +2,72 @@ package com.example.springbootlab.handler;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Base64;
+
+import static org.assertj.core.api.Assertions.*;
 
 class JwtHandlerTest {
     JwtHandler jwtHandler = new JwtHandler();
 
+    @Test
+    void createTokenTest() {
+        // given 토큰을 생성할 때 사용할 키 생성
+        String encodedKey = createEncodedKey();
+        String subject = "subject"; // jwt에 담을 내용
 
+        // when
+        String token = createToken(encodedKey, subject, 0L);
+
+        // then
+        assertThat(token).contains("Bearer");
+    }
+
+    @Test
+    void extractSubjectTest() {
+        // given
+        String encodedKey = createEncodedKey();
+        String subject = "subject";
+        String token = createToken(encodedKey, subject, 60L);
+
+        // when
+        String extractedSubject = jwtHandler.extractSubject(encodedKey, token);
+
+        // then
+        assertThat(extractedSubject).isEqualTo(subject);
+    }
+
+    @Test
+    void invalidateByInvalidKeyTest() { // 토큰에 생성되었던 키 외에, 다른 키를 주었을 때 유효안한지?
+        // given
+        String encodedKey = createEncodedKey();
+        String token = createToken(encodedKey, "subject", 60L);
+
+        // when
+        boolean isValid = jwtHandler.validate("invalid", token);
+
+        // then
+        assertThat(isValid).isFalse();
+    }
+
+    @Test
+    void invalidateByExpiredTokenTest() {
+        // given
+        String encodedKey = createEncodedKey();
+        String token = createToken(encodedKey, "subject", 0L); // 만료 기간을 0으로 설정 후 유효한지 검사
+
+        // when
+        boolean isValid = jwtHandler.validate(encodedKey, token);
+
+        // then
+        assertThat(isValid).isFalse();
+    }
+
+    private String createEncodedKey(){
+        return Base64.getEncoder().encodeToString("myKey".getBytes());
+    }
+
+    private String createToken(String encodedKey, String subject, long maxAgeSeconds) {
+        return jwtHandler.createToken(encodedKey, subject, maxAgeSeconds);
+    }
 
 }

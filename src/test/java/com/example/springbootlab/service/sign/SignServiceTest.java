4c3c57e2eb5d0899be988092046a3fcb584620e4
ohjinseo <ojs835@naver.com>
2022-03-13 22:06:16 +0900

    SignService 단위 테스트 작성

---
+++ src/test/java/com/example/springbootlab/service/sign/SignServiceTest.java
@@ -0,0 +1,135 @@
+package com.example.springbootlab.service.sign;
+
+import com.example.springbootlab.domain.member.*;
+import com.example.springbootlab.dto.sign.SignInRequest;
+import com.example.springbootlab.dto.sign.SignInResponse;
+import com.example.springbootlab.dto.sign.SignUpRequest;
+import com.example.springbootlab.exception.LoginFailureException;
+import com.example.springbootlab.exception.MemberEmailAlreadyExistsException;
+import com.example.springbootlab.exception.MemberNicknameAlreadyExistsException;
+import com.example.springbootlab.exception.RoleNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+// SignService으 코드만 테스트하기 위해 Mockito 사용
+@ExtendWith(MockitoExtension.class)
+class SignServiceTest {
+    @InjectMocks
+    SignService signService;
+    @Mock
+    MemberRepository memberRepository;
+    @Mock
+    RoleRepository roleRepository;
+    @Mock
+    PasswordEncoder passwordEncoder;
+    @Mock
+    TokenService tokenService;
+
+
+    @Test // 회원가입 테스트
+    void signUpTest() {
+        // given
+        SignUpRequest req = createSignUpRequest();
+        given(roleRepository.findByRoleType(RoleType.ROLE_NORMAL)).willReturn(Optional.of(new Role(RoleType.ROLE_NORMAL)));
+
+        // when
+        signService.signUp(req);
+
+        // then
+        verify(passwordEncoder).encode(req.getPassword());
+        verify(memberRepository).save(any());
+    }
+
+    @Test // 회원가입 시 이메일 중복 테스트
+    void validateSignUpByDuplicateEmailTest() {
+        // given
+        given(memberRepository.existsByEmail(anyString())).willReturn(true);
+
+        // when, then
+        assertThatThrownBy(() -> signService.signUp(createSignUpRequest())).isInstanceOf(MemberEmailAlreadyExistsException.class);
+    }
+
+    @Test // 회원가입 시 닉네임 중복 테스트
+    void validateSignUpByDuplicateNicknameTest() {
+        // given
+        given(memberRepository.existsByNickname(anyString())).willReturn(true);
+
+        assertThatThrownBy(() -> signService.signUp(createSignUpRequest())).isInstanceOf(MemberNicknameAlreadyExistsException.class);
+    }
+
+    @Test // 등록되지 않은 권한 등급으로 회원가입 테스트
+    void signUpRoleNotFoundTest() {
+        // given
+        given(roleRepository.findByRoleType(RoleType.ROLE_NORMAL)).willReturn(Optional.empty());
+
+        // when, then
+        assertThatThrownBy(() -> signService.signUp(createSignUpRequest())).isInstanceOf(RoleNotFoundException.class);
+    }
+
+    @Test
+    void signInTest() {
+        // given
+        given(memberRepository.findByEmail(any())).willReturn(Optional.of(createMember()));
+        given(passwordEncoder.matches(anyString(), anyString())).willReturn(true);
+        given(tokenService.createAccessToken(anyString())).willReturn("access");
+        given(tokenService.createRefreshToken(anyString())).willReturn("refresh");
+
+        // when
+        SignInResponse res = signService.signIn(new SignInRequest("email", "password"));
+
+        // then
+        assertThat(res.getAccessToken()).isEqualTo("access");
+        assertThat(res.getRefreshToken()).isEqualTo("refresh");
+    }
+
+    @Test
+    void signInExceptionByNoneMemberTest() {
+        // given
+        given(memberRepository.findByEmail(any())).willReturn(Optional.empty());
+
+        // when, then
+        assertThatThrownBy(() -> signService.signIn(new SignInRequest("email", "password"))).isInstanceOf(LoginFailureException.class);
+    }
+
+    @Test
+    void signInExceptionByInvalidPasswordTest() {
+        // given
+        given(memberRepository.findByEmail(any())).willReturn(Optional.of(createMember()));
+        given(passwordEncoder.matches(anyString(), anyString())).willReturn(false);
+
+        // when, then
+        assertThatThrownBy(() -> signService.signIn(new SignInRequest("email", "password")))
+                .isInstanceOf(LoginFailureException.class);
+    }
+
+
+    private SignUpRequest createSignUpRequest() {
+        return new SignUpRequest("email", "password", "username", "nickname");
+    }
+
+    private Member createMember() {
+        return Member.builder()
+                .email("email")
+                .password("password")
+                .username("username")
+                .nickname("nickname")
+                .roles(emptyList()).build();
+    }
+}

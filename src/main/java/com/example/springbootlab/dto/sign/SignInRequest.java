7559ce129066488f992ceccead67cbfe16c3ac1b
ohjinseo <ojs835@naver.com>
2022-03-15 01:10:24 +0900

    bug fix : 테스트코드에서 objectmapper로 json으로 변환하기 위해 기본 생성자 추가

--- src/main/java/com/example/springbootlab/dto/sign/SignInRequest.java
+++ src/main/java/com/example/springbootlab/dto/sign/SignInRequest.java
@@ -2,9 +2,13 @@ package com.example.springbootlab.dto.sign;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor 
+// bug fix : 테스트코드에서 objectMapper로 JSON으로 변환하는 과정에서 기본 생성자가 필요하므로 @NoArgsConstructor 추가 [InvalidDefinitionException]
 public class SignInRequest {
     private String email;
     private String password;

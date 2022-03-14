4682db90b5e5c69f86905c951abd07569b57ea0d
ohjinseo <ojs835@naver.com>
2022-03-15 03:08:39 +0900

    RestAPI에서 발생하는 예외 글로벌하게 핸들링

---
+++ src/main/java/com/example/springbootlab/advice/ExceptionAdvice.java
@@ -0,0 +1,61 @@
+package com.example.springbootlab.advice;
+
+import com.example.springbootlab.controller.response.Response;
+import com.example.springbootlab.exception.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice // RestController가 적용된 Bean 내에서 발생하는 모든 예외 인터럽트
+@Slf4j
+public class ExceptionAdvice {
+    // ExceptionHandler는 발생하는 예외의 더 구체적인 것을 선택하므로
+    // 다른 Exception을 잡지 못하면 여기로 오게됨
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 상태코드 500
+    public Response exception(Exception e) {
+        log.info("e = {}", e.getMessage());
+        return Response.failure(-1000, "오류가 발생하였습니다");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        // 요청 객체의 validation을 수행할 때, 해당 예외 발생
+        // 각 검증 어노테이션 별로 지정했던 메시지 응답
+        return Response.failure(-1003, e.getBindingResult().getFieldError().getDefaultMessage());
+    }
+
+    @ExceptionHandler(LoginFailureException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public Response loginFailureException() {
+        return Response.failure(-1004, "로그인에 실패하였습니다.");
+    }
+
+    @ExceptionHandler(MemberEmailAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Response memberEmailAlreadyExistsException(MemberEmailAlreadyExistsException e){
+        return Response.failure(-1005, e.getMessage() + "은 중복된 이메일 입니다.");
+    }
+
+    @ExceptionHandler(MemberNicknameAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Response memberNicknameAlreadyExistsException(MemberNicknameAlreadyExistsException e){
+        return Response.failure(-1006, e.getMessage() + "은 중복된 닉네임 입니다.");
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response memberNotFoundException(){
+        return Response.failure(-1007, "요청한 회원을 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(RoleNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response roleNotFoundException(){
+        return Response.failure(-1007, "요청한 권한 등급을 찾을 수 없습니다.");
+    }
+}
